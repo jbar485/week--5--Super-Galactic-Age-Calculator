e5bab69b821a300c70591a9dec88d0cead248512
Havalah Carlson <havalah@Havalahs-MacBook-Pro.local>
2019-11-15 10:03:17 -0800

    add finished class

--- src/scripts.js
+++ src/scripts.js
@@ -5,16 +5,16 @@ export class Person {
   }
 
   checkType() {
-    // let result = "";
-    // if (this.sidea <= 0 || this.sideb <= 0 || this.sidec <= 0) {
-    //   result = "NOT A TRIANGLE!!!!";
-    // }else if (this.sidea === this.sideb && this.sideb === this.sidec) {
-    //   result = "You have an Equilateral triangle";
-    // }else if (this.sidea === this.sideb || this.sidea === this.sidec || this.sideb === this.sidec) {
-    //   result = "You have an Isosceles triangle";
-    // }else if (this.sidea != this.sideb && this.sidea != this.sidec && this.sidec != this.sideb) {
-    //   result = "You have an scalene triangle";
-    // }
-    //   return result;
- }
+ //    let result = "";
+ //    if (this.age < this.life) {
+ //      result = "NOT A TRIANGLE!!!!";
+ //    }else if (this.sidea === this.sideb && this.sideb === this.sidec) {
+ //      result = "You have an Equilateral triangle";
+ //    }else if (this.sidea === this.sideb || this.sidea === this.sidec || this.sideb === this.sidec) {
+ //      result = "You have an Isosceles triangle";
+ //    }else if (this.sidea != this.sideb && this.sidea != this.sidec && this.sidec != this.sideb) {
+ //      result = "You have an scalene triangle";
+ //    }
+ //      return result;
+ // }
 }
