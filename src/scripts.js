af8af4c080b5d726299408ab56a9199055b2dcab
Havalah Carlson <havalah@Havalahs-MacBook-Pro.local>
2019-11-15 13:59:20 -0800

    add code to make this work better

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,12 @@ export class Person {
     this.mercury = 0,
     this.venus = 0,
     this.mars = 0,
-    this.jupiter = 0
+    this.jupiter = 0,
+    this.earthLife = 0,
+    this.mercuryLife = 0,
+    this.venusLife = 0,
+    this.marsLife = 0,
+    this.jupiterLife = 0
   }
 
   earthAge() {
@@ -37,6 +42,27 @@ export class Person {
     this.jupiter = this.age * 11.86;
     const jupiter = Math.floor(this.jupiter);
     return jupiter;
+  }
+
+  lifeExpectancy() {
+    let lifeString = "";
+    if (this.age === this.life) {
+      lifeString = "Wow your Life expectancy is perfect, good luck moving forward"
+    }else if (this.age > this.life) {
+      this.earthLife = 0,
+      this.mercuryLife = 0,
+      this.venusLife = 0,
+      this.marsLife = 0,
+      this.jupiterLife = 0
+      lifeString
+    }else if (this.age < this.life) {
+      this.earthLife = this.life - this.earth;
+      this.mercuryLife = this.life - this.mercury;
+      this.venusLife = this.life - this.life;
+      this.marsLife = this.life - this.mars;
+      this.jupiterLife = this.life - this.jupiter;
+    }
+    return lifeString
   }
 
 }
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -43,15 +43,3 @@ describe("prototype Jupiter", () => {
       expect(person.jupiterAge()).toEqual(142);
   });
 });
-  // test("Should be Equilateral", () => {
-  //   var result = new MyTriangle(2,2,2);
-  //   expect(result.checkType()).toEqual("You have an Equilateral triangle");
-  // });
-  // test("Should be Isosceles", () => {
-  //   var result = new MyTriangle(2,2,3);
-  //   expect(result.checkType()).toEqual("You have an Isosceles triangle");
-  // });
-  // test("Should be scalene", () => {
-  //   var result = new MyTriangle(2,3,4);
-  //   expect(result.checkType()).toEqual("You have an scalene triangle");
-  // });
