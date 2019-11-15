266306a9edf67554042d6d2cfcdfddb6f66258c0
Havalah Carlson <havalah@Havalahs-MacBook-Pro.local>
2019-11-15 11:19:14 -0800

    add prototype mercury test

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -16,6 +16,14 @@ describe("prototype Earth", () => {
       expect(person.earthAge()).toEqual(12);
   });
 });
+
+describe("prototype Mercury", () => {
+
+  test('should record Mercury age', () => {
+      var person = new Person(12, 45);
+      expect(person.mercuryAge()).toEqual(2.88);
+  });
+});
   // test("Should be Equilateral", () => {
   //   var result = new MyTriangle(2,2,2);
   //   expect(result.checkType()).toEqual("You have an Equilateral triangle");
