e4a68f280d2fc7510e2cc5fe7a2df665c264d2c0
Havalah Carlson <havalah@Havalahs-MacBook-Pro.local>
2019-11-15 10:43:47 -0800

    add javascript to update my class and rerun test

--- src/scripts.js
+++ src/scripts.js
@@ -1,20 +1,20 @@
 export class Person {
   constructor(age, life) {
     this.age = age,
-    this.life = life
+    this.life = life,
+    this.earth = 0,
+    this.mercury = 0,
+    this.venus = 0,
+    this.mars = 0,
+    this.jupiter = 0
   }
 
   checkType() {
- //    let result = "";
- //    if (this.age < this.life) {
- //      result = "NOT A TRIANGLE!!!!";
- //    }else if (this.sidea === this.sideb && this.sideb === this.sidec) {
- //      result = "You have an Equilateral triangle";
- //    }else if (this.sidea === this.sideb || this.sidea === this.sidec || this.sideb === this.sidec) {
- //      result = "You have an Isosceles triangle";
- //    }else if (this.sidea != this.sideb && this.sidea != this.sidec && this.sidec != this.sideb) {
- //      result = "You have an scalene triangle";
- //    }
- //      return result;
+    const result = "";
+    if (this.age < this.life) {
+      result = "";
+    }
+
+      return result;
  }
 }
